ab4711742a114a360b314eadbe864315312d7d66
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-24 01:15:01 -0500

    [Ruchita] Add: Flip Coin program using random function

---
+++ Basic_Core_Programs/FlipCoin.cs
@@ -0,0 +1,28 @@
+﻿namespace Basic_Core_Programs
+{
+    internal class FlipCoin
+    {
+        //flip coin and print percentage of Heads and Tail.
+        public static void flipCoin()
+        {
+           
+          float coinflip;
+            //random function for coinflip
+            Random r1 = new Random();
+            coinflip = r1.Next();
+            
+            
+            if (r1.Next(2) == 0)
+            {
+                Console.WriteLine("Coin Flip at Head Side");
+            }
+            else 
+            {
+                Console.WriteLine("Coin Flip at Tail Side");
+            }
+           // int percent = (heads / 10) * 100;
+      //Console.WriteLine(percent);
+        }
+
+    }
+}
---
+++ Basic_Core_Programs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Basic_Core_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoin.flipCoin();
+        }
+    }
+}
